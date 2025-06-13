7426dfb359b32aa7dd9ad5b8dc0e581316b82d6a
bobby3039 <harsh102938@gmail.com>
2025-06-14 00:43:23 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const BACKEND_URL =  'REACT_APP_BACKEND_URL';
+const BACKEND_URL =  'process.env.REACT_APP_BACKEND_URL';
 
 //console.log('Backend URL:', BACKEND_URL);
 //console.log('Environment variable:', process.env.REACT_APP_BACKEND_URL);
