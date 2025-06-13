8c2ff48739d0cc132eec43044ca02f556a97baab
bobby3039 <harsh102938@gmail.com>
2025-06-13 22:04:34 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,8 +6,8 @@ const MONGODB_URI = process.env.MONGODB_URI;
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
+     // useNewUrlParser: true,
+      //useUnifiedTopology: true
     });
     console.log(`MongoDB Atlas Connected: ${conn.connection.host}`);
   } catch (error) {
